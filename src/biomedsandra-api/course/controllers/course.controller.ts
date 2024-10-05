aac6d4057960f1ea267118dcdd4a22e7713b7c9c
WELLITOn07 <w.elliton2010@hotmail.com>
2024-10-05 19:18:52 -0300

    Remove environment files from version control

--- src/biomedsandra-api/course/controllers/course.controller.ts
+++ src/biomedsandra-api/course/controllers/course.controller.ts
@@ -9,18 +9,11 @@ import {
 } from '@nestjs/common';
 import { Course, Prisma } from '@prisma/client';
 import { CourseService } from '../services/course.service';
+import { ParamStringId } from 'src/public/decorators/param-string-id.decorator';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
-
-  @Get('health-check')
-  async healthCheck(): Promise<any> {
-    return {
-      status: 'ok',
-    };
-  }
-  
   @Post()
   async createCourse(@Body() data: Prisma.CourseCreateInput): Promise<Course> {
     return this.courseService.createCourse(data);
@@ -32,20 +25,20 @@ export class CourseController {
   }
 
   @Get(':id')
-  async getCourseById(@Param('id') id: string): Promise<Course | null> {
+  async getCourseById(@ParamStringId() id: string): Promise<Course | null> {
     return this.courseService.getCourseById(id);
   }
 
   @Put(':id')
   async updateCourse(
-    @Param('id') id: string,
+    @ParamStringId() id: string,
     @Body() data: Prisma.CourseUpdateInput,
   ): Promise<Course> {
     return this.courseService.updateCourse(id, data);
   }
 
   @Delete(':id')
-  async deleteCourse(@Param('id') id: string): Promise<Course> {
+  async deleteCourse(@ParamStringId() id: string): Promise<Course> {
     return this.courseService.deleteCourse(id);
   }
 }
