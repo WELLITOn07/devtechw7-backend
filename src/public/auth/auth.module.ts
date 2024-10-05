944a14026f5be91c1667b668285cdc763fc358e4
WELLITOn07 <w.elliton2010@hotmail.com>
2024-10-05 20:06:41 -0300

    fix(packge.jgon): jwt types

--- src/public/auth/auth.module.ts
+++ src/public/auth/auth.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [JwtModule.register({
-    secret: process.env.JWT_SECRET,
-    signOptions: { expiresIn: '1d' },
-  })],
+    
 })
 export class AuthModule {}
