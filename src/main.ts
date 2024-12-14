0a20494ddf1947f7d344908d2f2208cfd90dd998
WELLITOn07 <w.elliton2010@hotmail.com>
2024-12-14 02:10:31 -0300

    fix: add http://localhost:4200 in the cors

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
       ];
       const allowedOriginsDev = [
         'http://localhost:8080',
-        'http://localhost:3000',
+        'http://localhost:4200',
       ];
 
       const isDevelopment = process.env.NODE_ENV === 'development';
