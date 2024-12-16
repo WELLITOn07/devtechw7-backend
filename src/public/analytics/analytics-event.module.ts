2b23aa67fe8843e1972f8db935fa2b55cd819de7
WELLITOn07 <w.elliton2010@hotmail.com>
2024-12-16 00:01:59 -0300

    feat: add auth to the analytics

--- src/public/analytics/analytics-event.module.ts
+++ src/public/analytics/analytics-event.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AnalyticsEventController } from './_controllers/analytics-event.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AnalyticsEventService } from './_services/analytics-event.service';
+import { RuleAccessGuard } from '../auth/_guards/rule-access.guard';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [AnalyticsEventController],
-  providers: [AnalyticsEventService, PrismaService],
+  providers: [AnalyticsEventService, PrismaService, RuleAccessGuard],
 })
 export class AnalyticsEventModule {}
